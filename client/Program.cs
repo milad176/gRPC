1b34b1c25d8c7c5539b2cbabb728eca72fee6712
Milad <Milad.kardgar1@gmail.com>
2023-05-02 22:14:37 +0200

    gRPC server & client setup

---
+++ client/Program.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        const string target = "127.0.0.1:50051";
+
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel(target, ChannelCredentials.Insecure);
+
+            channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                    Console.WriteLine("The client connected successfully");
+            });
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,41 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    class Program
+    {
+        const int port = 50051;
+        static void Main(string[] args)
+        {
+            Server server = null;
+
+            try
+            {
+                server = new Server()
+                {
+                    Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+                };
+
+                server.Start();
+                Console.WriteLine("The server is listening to the port :" + port);
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The server failed to start :" + e.Message);
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                    server.ShutdownAsync().Wait();
+            }
+        }
+    }
+}
